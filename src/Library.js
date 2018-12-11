0f67d45d48423d0094bebd9febff5eeac5659177
Rudy <rudypichola@gmail.com>
2018-12-10 21:15:14 -0600

    Update Library.js

---
+++ src/Library.js
@@ -0,0 +1,23 @@
+/*
+* JavaScript Library Simulator
+* @Author Rudy L. Pichola-Flores
+* Monday December 10, 2018
+*/
+
+//Main Parent Class
+class Media {
+
+}
+
+//Child Classes of Media
+class Book {
+
+}
+
+class Movie {
+
+}
+
+class CD {
+	
+}
