deb12d102234befb9a51d68831752479b9354314
Rudy <rudypichola@gmail.com>
2018-12-10 21:24:07 -0600

    Update Library.js

--- src/Library.js
+++ src/Library.js
@@ -6,12 +6,50 @@
 
 //Main Parent Class
 class Media {
-
+  
 }
 
 //Child Classes of Media
 class Book {
+  constructor(author, title, pages) {
+    this._author = author;
+    this._title = title;
+    this._pages = pages;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
 
+  get author() {
+  	return this._author;
+  }
+
+  get title() {
+  	return this._title;
+  }
+
+  get pages() {
+  	return this._pages;
+  }
+
+  get isCheckedOut() {
+  	return this._isCheckedOut;
+  }
+
+  get ratings() {
+  	return this._ratings;
+  }
+
+  getAverageRating() {
+
+  }
+
+  toggleCheckOutStatus() {
+
+  }
+
+  addRating() {
+  	
+  }
 }
 
 class Movie {
@@ -19,5 +57,5 @@ class Movie {
 }
 
 class CD {
-	
+
 }
